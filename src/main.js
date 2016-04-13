fd3941519dab21542d09af115fd19c16259fac0b
Guido Lorenz <guido.lorenz@artcom.de>
2016-04-13 16:13:37 +0200

    Restructure async code using co/generators

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const bunyan = require("bunyan")
+const co = require("co")
 const express = require("express")
 const git = require("nodegit")
 const promisify = require("promisify-node")
@@ -14,12 +15,12 @@ const log = bunyan.createLogger({ name: "git-json-api" })
 const CONFIGURATION_REPO = process.env.CONFIGURATION_REPO
 
 app.get("/master", (req, res) => {
-  tmpDirAsync().then((dir) =>
-    git.Clone.clone(CONFIGURATION_REPO, dir, { bare: 1 })
-  ).then((repo) => {
-    const path = repo.path()
-    res.json({ path })
-    return rimrafAsync(path)
+  co(function* () {
+    const dir = yield tmpDirAsync()
+    const repo = yield git.Clone.clone(CONFIGURATION_REPO, dir, { bare: 1 })
+    const commit = yield repo.getBranchCommit("master")
+    res.json({ master: commit.sha() })
+    yield rimrafAsync(repo.path())
   })
 })
 
