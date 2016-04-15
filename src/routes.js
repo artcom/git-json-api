cef44960c8a939a8329f20aae8b85115f9b5769a
Guido Lorenz <guido.lorenz@artcom.de>
2016-04-15 16:07:57 +0200

    Add config route

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,19 @@ import { usingRepo } from "./repo"
 
 export default new express.Router()
   .get("/latest", usingRepo(getMaster))
+  .get("/config/:version", usingRepo(getConfig))
 
 async function getMaster(repo, req, res) {
   const commit = await repo.getMasterCommit()
   res.json({ version: commit.sha() })
+}
+
+async function getConfig(repo, req, res) {
+  try {
+    const version = req.params.version
+    const commit = await repo.getCommit(version)
+    res.json({ version: commit.sha() })
+  } catch (error) {
+    res.status(404).end()
+  }
 }
