c8dc5d9800fb207e89d15be701c964c7387f340c
Guido Lorenz <guido.lorenz@artcom.de>
2016-04-19 09:01:47 +0200

    Return complete repo content as one JSON object

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path"
 
 import { usingRepo } from "./repo"
 
@@ -15,8 +16,31 @@ async function getConfig(repo, req, res) {
   try {
     const version = req.params.version
     const commit = await repo.getCommit(version)
-    res.json({ version: commit.sha() })
+    const tree = await commit.getTree()
+    const data = await treeToObject(tree)
+    res.json(data)
   } catch (error) {
     res.status(404).end()
+  }
+}
+
+async function treeToObject(tree) {
+  const result = {}
+
+  for (const entry of tree.entries()) {
+    const key = path.basename(entry.path(), ".json")
+    result[key] = await entryToObject(entry)
+  }
+
+  return result
+}
+
+async function entryToObject(entry) {
+  if (entry.isTree()) {
+    const subTree = await entry.getTree()
+    return treeToObject(subTree)
+  } else if (entry.isBlob()) {
+    const blob = await entry.getBlob()
+    return blob.toString()
   }
 }
