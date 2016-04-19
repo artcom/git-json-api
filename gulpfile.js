c0098b918e8cbfb6c7e2fb0084f06a32a33fa413
Guido Lorenz <guido.lorenz@artcom.de>
2016-04-19 13:50:56 +0200

    Pipe server output through bunyan during development

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
+"use strict"
+
 const gulp = require("gulp")
 const babel = require("gulp-babel")
 const nodemon = require("gulp-nodemon")
 const path = require("path")
+const spawn = require("child_process").spawn
 
 const SOURCE_DIR = "./src"
 const DIST_DIR = "./dist"
+
+let bunyan = null
 
 gulp.task("compile", () =>
   gulp.src(path.join(SOURCE_DIR, "*.js"))
@@ -16,6 +21,22 @@ gulp.task("watch", ["compile"], () => {
   nodemon({
     script: path.join(DIST_DIR, "main.js"),
     watch: SOURCE_DIR,
-    task: ["compile"]
+    task: ["compile"],
+    stdout: false
+  }).on("readable", function () {
+    if (bunyan) {
+      bunyan.kill()
+    }
+
+    bunyan = spawn("./node_modules/.bin/bunyan", [
+      "--output", "short",
+      "--color"
+    ])
+
+    bunyan.stdout.pipe(process.stdout)
+    bunyan.stderr.pipe(process.stderr)
+
+    this.stdout.pipe(bunyan.stdin)
+    this.stderr.pipe(bunyan.stdin)
   })
 })
