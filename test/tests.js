0c7dd0a1065bc75fcb3abfb5f83314ad5e8d638a
Guido Lorenz <guido.lorenz@artcom.de>
2016-05-10 10:48:03 +0200

    Test updatePath()

--- test/tests.js
+++ test/tests.js
@@ -8,6 +8,7 @@ import tmp from "tmp"
 import getLatestVersion from "../src/actions/getLatestVersion"
 import getPath from "../src/actions/getPath"
 import getRoot from "../src/actions/getRoot"
+import updatePath from "../src/actions/updatePath"
 
 import { fetchRepo } from "../src/repo"
 
@@ -44,7 +45,7 @@ describe("Git JSON API", function() {
       writeFileSync(absPath, JSON.stringify(content))
       git("add", filePath)
       git("commit", "--message", `Add ${filePath}`)
-      this.versions.push(git("show-ref", "--hash", "master"))
+      this.versions.push(git("show-ref", "--hash", "refs/heads/master"))
     }
 
     git("init", "--bare", upstreamDir)
@@ -114,6 +115,26 @@ describe("Git JSON API", function() {
     it("returns content of a file", function() {
       return getPath(this.repo, { version: last(this.versions), 0: "dirB/x/file" }).then((data) => {
         expect(data).to.deep.equal(fileBx)
+      })
+    })
+  })
+
+  describe("updatePath", function() {
+    it("writes changes to a file", function() {
+      const params = { version: last(this.versions), 0: "dirA" }
+      const body = {
+        file1: {
+          foo: "bar",
+          number: 2
+        }
+      }
+
+      return updatePath(this.repo, params, body).then((result) => {
+        expect(result).to.have.property("version")
+
+        return getPath(this.repo, { version: result.version, 0: "dirA" }).then((data) => {
+          expect(data).to.deep.equal(body)
+        })
       })
     })
   })
