d0b55f7530c370504cb57a597fb45e14f50a9a45
Guido Lorenz <guido.lorenz@artcom.de>
2016-05-10 13:59:52 +0200

    Create explicit merge commits

--- src/actions/updatePath.js
+++ src/actions/updatePath.js
@@ -1,73 +1,74 @@
 import Git from "nodegit"
 
+import getLatestVersion from "./getLatestVersion"
 import { getSchema, isFile } from "./helpers"
 
 export default async function updatePath(repo, params, data) {
   const version = params.version
-  console.log(`Changed: ${version}`)
-
   const path = params[0]
 
-  const masterCommit = await repo.getMasterCommit()
-  console.log(`Master version: ${masterCommit}`)
+  const parentCommit = await repo.getCommit(version)
+  const parentTree = await parentCommit.getTree()
 
-  const masterTree = await masterCommit.getTree()
-  const schema = await getSchema(masterTree)
+  const schema = await getSchema(parentTree)
   const newSubTreeOid = await objectToTree(data, path, repo, schema)
 
-  const builder = await Git.Treebuilder.create(repo, masterTree)
+  const builder = await Git.Treebuilder.create(repo, parentTree)
   await builder.remove(path)
   await builder.insert(path, newSubTreeOid, Git.TreeEntry.FILEMODE.TREE)
   const newTreeOid = builder.write()
   const newTree = await Git.Tree.lookup(repo, newTreeOid)
 
-  let diff
-  if (masterCommit.sha() !== version) {
-    const ancestorCommit = await repo.getCommit(version)
-    const ancestorTree = await ancestorCommit.getTree()
-    const index = await Git.Merge.trees(
-      repo, ancestorTree, newTree, masterTree, new Git.MergeOptions()
-    )
-    if (index.hasConflicts()) {
-      console.log("Index has conflicts")
-      throw new Error("Index has conflicts")
-    } else {
-      console.log("Write merged tree")
-      const mergedTreeOid = await index.writeTreeTo(repo)
-      console.log("Get merged tree")
-      const mergedTree = await Git.Tree.lookup(repo, mergedTreeOid)
-      console.log("Diff trees")
-      diff = await Git.Diff.treeToTree(repo, masterTree, mergedTree)
-    }
-  } else {
-    diff = await Git.Diff.treeToTree(repo, masterTree, newTree)
+  const diff = await Git.Diff.treeToTree(repo, parentTree, newTree)
+  if (diff.numDeltas() === 0) {
+    return {}
   }
 
+  const latest = await getLatestVersion(repo)
+  let newOid
 
-  if (diff.numDeltas() > 0) {
-    const commitOid = await repo.createCommit(
+  if (version === latest.version) {
+    newOid = await repo.createCommit(
       "refs/heads/master",
       repo.defaultSignature(),
       repo.defaultSignature(),
       `Update ${path}`,
       newTreeOid,
-      [masterCommit]
+      [parentCommit]
+    )
+  } else {
+    const commitOid = await repo.createCommit(
+      null,
+      repo.defaultSignature(),
+      repo.defaultSignature(),
+      `Update ${path}`,
+      newTreeOid,
+      [parentCommit]
     )
 
-    const remote = await repo.getRemote("origin")
-    const errorCode = await remote.push("refs/heads/master:refs/heads/master")
+    const masterCommit = await repo.getCommit(latest.version)
+    const commit = await repo.getCommit(commitOid)
+    const index = await Git.Merge.commits(repo, masterCommit, commit)
+    const mergeTreeOid = await index.writeTreeTo(repo)
 
-    if (errorCode) {
-      console.log(errorCode)
-      throw new Error(errorCode)
-    } else {
-      const commit = await Git.Commit.lookup(repo, commitOid)
-      return { version: commit.sha() }
-    }
-  } else {
-    console.log("Nothing changed")
-    return {}
+    newOid = await repo.createCommit(
+      "refs/heads/master",
+      repo.defaultSignature(),
+      repo.defaultSignature(),
+      `Merge ${path}`,
+      mergeTreeOid,
+      [masterCommit, commit]
+    )
   }
+
+  const remote = await repo.getRemote("origin")
+  const errorCode = await remote.push("refs/heads/master:refs/heads/master")
+
+  if (errorCode) {
+    throw new Error(errorCode)
+  }
+
+  return { version: newOid.toString() }
 }
 
 export async function objectToTree(object, path, repo, schema) {
@@ -83,18 +84,10 @@ export async function objectToTree(object, path, repo, schema) {
       const filename = `${key}.json`
       const buffer = new Buffer(`${JSON.stringify(object[key], null, 2)}\n`)
       const blobOid = Git.Blob.createFromBuffer(repo, buffer, buffer.length)
-      try {
-        await builder.insert(filename, blobOid, Git.TreeEntry.FILEMODE.BLOB)
-      } catch (e) {
-        console.log(e)
-      }
+      await builder.insert(filename, blobOid, Git.TreeEntry.FILEMODE.BLOB)
     } else {
-      try {
-        const subTreeOid = await objectToTree(object[key], childPath, repo, schema)
-        await builder.insert(key, subTreeOid, Git.TreeEntry.FILEMODE.TREE)
-      } catch (e) {
-        console.log(e)
-      }
+      const subTreeOid = await objectToTree(object[key], childPath, repo, schema)
+      await builder.insert(key, subTreeOid, Git.TreeEntry.FILEMODE.TREE)
     }
   }
 
