9f52fb39d77ea1656d748cc09653c661942a92cc
Mirko Geissler <mirko.geissler@artcom.de>
2019-10-02 08:55:00 +0200

    Make unknown branch/commit errors consistent

--- src/repo.js
+++ src/repo.js
@@ -27,7 +27,7 @@ module.exports = class Repo {
       await this.lock.lock()
 
       await this.repo.fetch("origin")
-      const commit = await getCommit(this.repo, version)
+      const commit = await getCommitByHashOrReference(this.repo, version)
       await this.cache.update(commit)
 
       this.lock.unlock()
@@ -46,8 +46,8 @@ module.exports = class Repo {
     try {
       await this.lock.lock()
 
-      const parentCommit = await this.repo.getCommit(parentCommitHash)
-      const branchCommit = await this.repo.getReferenceCommit(`refs/remotes/origin/${branch}`)
+      const parentCommit = await getCommitByHash(this.repo, parentCommitHash)
+      const branchCommit = await getCommitByReference(this.repo, branch)
       const newTreeOid = await writeFiles(this.repo, parentCommit, path, files)
       const commitHash = await commitAndMerge(this.repo, parentCommit, branchCommit, newTreeOid, `Update '/${path}'`)
       await pushHeadToOrigin(this.repo, branch)
@@ -62,10 +62,20 @@ module.exports = class Repo {
   }
 }
 
-async function getCommit(repo, version) {
+async function getCommitByHash(repo, hash) {
+  return repo.getCommit(hash)
+    .catch(() => { throw new Error(`Commit not found: '${hash}'`) })
+}
+
+async function getCommitByReference(repo, reference) {
+  return repo.getReferenceCommit(`refs/remotes/origin/${reference}`)
+    .catch(() => { throw new Error(`Reference not found: '${reference}'`) })
+}
+
+async function getCommitByHashOrReference(repo, version) {
   return repo.getReferenceCommit(`refs/remotes/origin/${version}`)
     .catch(() => repo.getCommit(version))
-    .catch(() => { throw new Error(`Branch or commit not found: '${version}'`) })
+    .catch(() => { throw new Error(`Reference or commit not found: '${version}'`) })
 }
 
 async function writeFiles(repo, parentCommit, path, files) {
