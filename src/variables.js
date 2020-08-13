428f341b7b1bccb34acf19e563d4c0a1b5dfb58d
Mirko Geissler <mirko.geissler@artcom.de>
2020-08-13 10:08:28 +0200

    Revert "Rename variable to backendUrl"
    This reverts commit a31dfdbe2d40585634d03861697c405b2ec996e9.

--- src/variables.js
+++ src/variables.js
@@ -1,15 +1,11 @@
-module.exports.replaceVariablesWithValues = content => {
-  if (process.env.BACKEND_URL) {
-    return content.replace(/\${backendUrl}/g, process.env.BACKEND_URL)
-  } else {
-    return content
-  }
-}
+module.exports.replaceVariablesWithValues = content => process.env.BACKEND_HOST
+  ? content.replace(/\${backendHost}/g, process.env.BACKEND_HOST)
+  : content
 
 module.exports.replaceValuesWithVariables = content => {
-  if (process.env.BACKEND_URL) {
-    const regExp = new RegExp(process.env.BACKEND_URL, "g")
-    return JSON.parse(JSON.stringify(content).replace(regExp, "${backendUrl}"))
+  if (process.env.BACKEND_HOST) {
+    const regExp = new RegExp(process.env.BACKEND_HOST, "g")
+    return JSON.parse(JSON.stringify(content).replace(regExp, "${backendHost}"))
   } else {
     return content
   }
